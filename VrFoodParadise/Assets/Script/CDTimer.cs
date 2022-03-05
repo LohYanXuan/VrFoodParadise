18cc40b53a1974c499fc0bda896ae11dc4016321
JayC42 <jcho99@1utar.my>
2022-03-05 22:37:47 +0800

    Countdown timer added
    Can use CDtimer to implement customer's patience clock

---
+++ VrFoodParadise/Assets/Script/CDTimer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A script to run the timer class
+/// </summary>
+public class CDTimer : MonoBehaviour
+{
+    Timer timer;
+    float startTime;
+
+    void Start()
+    {
+        // create and run timer
+        timer = gameObject.AddComponent<Timer>();
+        timer.Duration = 3;
+        timer.Run();
+
+        // save start time
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (timer.Finished)
+        {
+            float elapsedTime = Time.time - startTime;
+
+            // Call whatever method here
+            Debug.Log("Timer ran for " + elapsedTime + " seconds");
+
+            // save start time and restart timer
+            startTime = Time.time;
+            timer.Run();
+        }
+    }
+}
---
+++ VrFoodParadise/Assets/Script/Timer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    float totalSeconds = 0; 
+    float elapsedSeconds = 0; 
+    bool running = false; 
+    bool started = false; 
+
+    // Set duration of the timer
+    // Can only be set if timer isn't running
+    public float Duration
+    {
+        set 
+        {
+            if (!running)
+            {
+                totalSeconds = value; 
+            }
+        }
+    }
+
+    // Gets whether or not the timer has finished running
+    public bool Finished
+    {
+		get { return started && !running; } 
+	}
+
+	// Gets whether or not the timer is currently running
+    public bool Running
+    {
+		get { return running; }
+	}
+
+    public void Run()
+    {
+        if (totalSeconds > 0)
+        {
+            started = true; 
+            running = true; 
+            elapsedSeconds = 0; 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (running)
+        {
+            elapsedSeconds += Time.deltaTime; 
+            if(elapsedSeconds >= totalSeconds)
+            {
+                running = false; 
+            }
+        }
+    }
+}
