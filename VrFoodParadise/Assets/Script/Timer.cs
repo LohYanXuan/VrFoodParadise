42d39fc5b24372f44dd7a7ebced3bbab7a60595d
JayC42 <jcho99@1utar.my>
2022-03-13 09:48:45 +0800

    Customer list added
    this list should be ok

--- VrFoodParadise/Assets/Script/Timer.cs
+++ VrFoodParadise/Assets/Script/Timer.cs
@@ -1,59 +1,84 @@
-﻿using System.Collections;
+﻿using System.Net.Mime;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
-    float totalSeconds = 0; 
-    float elapsedSeconds = 0; 
-    bool running = false; 
-    bool started = false; 
+    [SerializeField] float totalSeconds = 0;
+    float elapsedSeconds = 0;
+    bool running = false;
+    //public int shuffle;
+    [SerializeField] int minOrder = 3, maxOrder = 7; 
+    [SerializeField] int minQ = 1,  maxQ = 5;
+  
+    //List is for customer to store & output their orders
+    // Orders are randomly churned. Randomness affects 
+    // quantity and ingredient type. 
+    // Burger ingredients: bun, sauce, tomatoes, lettuce, cheese, egg, meat
+    // Customer sld choose 5 ing minimum, 
+    List<string> ingredients = new List<string> { "Bun", "Sauce", "Tomato", "Meat", "Lettuce", "Cheese", "Egg" };
+    List<int> iQuantity = new List<int>();
 
-    // Set duration of the timer
-    // Can only be set if timer isn't running
-    public float Duration
+    void start()
     {
-        set 
-        {
-            if (!running)
-            {
-                totalSeconds = value; 
-            }
-        }
+        running = true;
     }
-
-    // Gets whether or not the timer has finished running
-    public bool Finished
-    {
-		get { return started && !running; } 
-	}
-
-	// Gets whether or not the timer is currently running
-    public bool Running
-    {
-		get { return running; }
-	}
-
-    public void Run()
-    {
-        if (totalSeconds > 0)
-        {
-            started = true; 
-            running = true; 
-            elapsedSeconds = 0; 
-        }
-    }
-
-    // Update is called once per frame
+        
     void Update()
     {
         if (running)
         {
-            elapsedSeconds += Time.deltaTime; 
-            if(elapsedSeconds >= totalSeconds)
+            elapsedSeconds += Time.deltaTime;
+            if (elapsedSeconds >= totalSeconds)
             {
-                running = false; 
+                running = false;
+                Run();
             }
         }
     }
+    void Run()
+    {
+        if (totalSeconds > 0)
+        { 
+            running = true;
+
+            
+
+            elapsedSeconds = 0;
+            PrintOrder(); 
+        }
+    }
+
+    /// <summary>
+    /// Get random items from ingredient list
+    /// Transfer all randomized items into customer order list
+    /// </summary>
+    /// <param name="ingredients"></param>
+    /// <param name="count"></param>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    List<T> GetRandomIngredients<T>(List<T> ingredients, int count)
+    {
+        List<T> cusOrder = new List<T>(); 
+        for(int i = 0; i < count; i++)
+        {
+            int index = Random.Range(0, ingredients.Count);
+            cusOrder.Add(ingredients[index]); 
+        }
+        return cusOrder; 
+    }
+
+    void PrintOrder()
+    {
+        // Randomize number of ingredients for burger
+        int iCount = Random.Range(minOrder, maxOrder);
+        var customerOrder = GetRandomIngredients(ingredients, iCount);
+        // Randomize Quantity for ingredients
+        int iQuantity = Random.Range(minQ, maxQ); 
+        Debug.Log("Amount: " + iQuantity);  
+        Debug.Log("All ingredients -> " + string.Join(", ", ingredients)); 
+        Debug.Log("Customer Orders -> " + string.Join(", ", customerOrder));
+    }
+
 }
