27c192a0b335d08a541c1a1a2f1f4736bc66be1e
LohYanXuan <86471228+LohYanXuan@users.noreply.github.com>
2022-05-06 12:38:22 +0800

    Main Menu
    Main Menu added.

---
+++ VrFoodParadise/Assets/Script/QuitGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    //Gaze at variable
+    [Header("GazeTime")]
+    [SerializeField] private float totalTime = 2;
+    bool gvrStatus;
+    float gvrTimer;
+    [SerializeField] private Image imgGaze;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gvrStatus = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+            if (gvrTimer >= totalTime)
+            {
+                Application.Quit();
+            }
+        }
+    }
+
+    public void GVROn()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+
+    }
+
+}
